052aa0eed2ea31103fb69b584432055bce520e5b
ValLR <valentinalopezrecabal@gmail.com>
2017-07-07 23:39:50 -0400

    función js sólo imprime

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-	alert("hola mundo");
+	  $.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        type: 'GET',
+        dataType: 'JSON',
+        data: {"limit": '811'},
+    })
+    
+    .done(function(res) {
+        console.log("success");
+        console.log(res);
+        //el for recorre todas las imagenes 
+        for (var i = 1; i < 719; i++) {
+            var pokeimg = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+            $(".poke-container").append(pokeimg);
+        }
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    })
 })
