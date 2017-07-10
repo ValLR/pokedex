542623fad30852c2200420a3d6a5001a6f1c16bf
ValLR <valentinalopezrecabal@gmail.com>
2017-07-10 07:27:08 -0400

    edit en estilos de container de cada pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,8 @@ $(document).ready(function(){
     .done(function(res) {
         console.log("success");
         console.log(res);
-        //el for recorre todas las imagenes 
         for (var i = 1; i < 719; i++) {
-            var pokeimg = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+            var pokeimg = $("<img src=https://pokeapi.co/media/img/" + i + ".png class='pi' id=" + i + ">");
             $(".poke-container").append(pokeimg);
         }
     })
